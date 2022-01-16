58d138346225ec31de62998182b364ba4744159a
arbiva <arbiva@post.bgu.ac.il>
2022-01-17 01:05:12 +0200

    rotate animation

--- tiktok_clone/src/VideoFooter/VideoFooter.js
+++ tiktok_clone/src/VideoFooter/VideoFooter.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from "react";
 import "./VideoFooter.css";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import Ticker from "react-ticker";
 
 function VideoFooter() {
-    return (
-        <div className='videoFooter'>
-            
+  return (
+    <div className="videoFooter">
+      <div className="videoFooterText">
+        <h3>hello</h3>
+        <p>this is text</p>
+        <div className="videoFooterTicker">
+          <MusicNoteIcon className="videoFooterIcon" />
+          <Ticker mode="smooth">
+            {({ index }) => (
+              <>
+                <p>i am a song</p>
+              </>
+            )}
+          </Ticker>
         </div>
-    )
+      </div>
+      <img className="videoFooterRecord"
+      src="http://static.thenounproject.com/png/934821-200.png"
+      alt=""
+      />
+    </div>
+  );
 }
 
-export default VideoFooter
+export default VideoFooter;
